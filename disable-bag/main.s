f459bc556a653c374bcc51ab105dae146bde5819
Kaiser de Emperana <kaisermauro8@gmail.com>
2017-12-16 15:49:08 -0300

    Make it easier to know which pointer to edit in main.s

--- disable-bag/main.s
+++ disable-bag/main.s
@@ -1,8 +1,11 @@
+
+	insert_at equ 0x08f00000
+
 	.gba
 	.thumb
 	.open input_game,output_game, 0x08000000
 
-	.org 0x08f00000
+	.org insert_at
 	.importobj relocatable_obj
 	
 	.include "patches/all.s"
